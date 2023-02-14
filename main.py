348f45974527b1694cdf0b810e449b4a8e0eeb56
bulbinator <116006041+bulbinator@users.noreply.github.com>
2023-02-14 10:25:27 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,24 +7,46 @@ import json
 from pathlib import Path
 import os
 from functions import *
+from webscrapper import *
 from discord import app_commands
 from discord.ext import commands
 from discord.app_commands import Choice
 from discord.ext import commands
+from keep_alive import keep_alive
 
 
 intents = discord.Intents.default()
+intents.typing = True
+intents.messages = True
+intents.message_content = True
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
 
 
 
-
 @client.event
 async def on_ready():
-    await tree.sync(guild=discord.Object(id=1072270741501907094))
+    await tree.sync()
     print("Ready!")
+
+@client.event
+async def on_message(message):
+  if 'https://thaqalayn.net/hadith' in message.content:
+    link = message.content.split()
+    for i in link:
+      if 'https://thaqalayn.net/hadith' in i:
+        hadith = linked(i)
+        embed = send(hadith)
+        await message.channel.send(embed=embed,reference=message,mention_author=False)
+        
+  if "http://shiaonlinelibrary.com/" in message.content:
+    link = message.content.split()
+    for i in link:
+      if 'http://shiaonlinelibrary.com/' in i:
+        hadith = sol(i)
+        embed = sol_send(hadith)
+        await message.channel.send(embed=embed,reference=message,mention_author=False)
 
 
 class Menu(discord.ui.View):
@@ -37,7 +59,7 @@ class Menu(discord.ui.View):
     await interaction.response.send_message("Hello you clicked me")
 
 
-@tree.command(name = "search", description = "Search for a hadith.", guild=discord.Object(id=1072270741501907094))
+@tree.command(name = "search", description = "Search for a hadith.")
 @app_commands.describe(keywords = "Enter the keywords of the hadith", lang = "English or Arabic?")
 @app_commands.choices(lang = [
   Choice(name = "English", value = "1"),
@@ -62,7 +84,7 @@ async def search(interaction: discord.Interaction, keywords: str, lang: str):
 
 
 
-@tree.command(name = "hadith", description = "Find a hadith.", guild=discord.Object(id=1072270741501907094))
+@tree.command(name = "hadith", description = "Find a hadith.")
 @app_commands.describe(lang = "English or Arabic?", book = "Enter book.", hadith_number = "Enter hadith number")
 @app_commands.choices(book = dict_choice(book_dict()))
 @app_commands.choices(lang = [
@@ -83,14 +105,14 @@ async def hadith(interaction: discord.Interaction, lang: str, book: str, hadith_
 
 
 
-@tree.command(name = "random", description = "Sends a random hadith.", guild=discord.Object(id=1072270741501907094))
+@tree.command(name = "random", description = "Sends a random hadith.")
 async def random(interaction: discord.Interaction):
   hadith = randomh()
   embed = send(hadith)
   await interaction.response.send_message(embed=embed)
   
 
-@tree.command(name = "link", description = "Returns the linked hadith.", guild=discord.Object(id=1072270741501907094))
+@tree.command(name = "link", description = "Returns the linked hadith.")
 @app_commands.describe(link = "Enter link.", lang = "English or Arabic?")
 @app_commands.choices(lang = [
   Choice(name = "English", value = "1"),
@@ -114,7 +136,7 @@ async def link(interaction: discord.Interaction, link: str, lang: str):
         
 
 
-@tree.command(name = "search_in", description = "Search for a hadith in a specific book.", guild=discord.Object(id=1072270741501907094))
+@tree.command(name = "search_in", description = "Search for a hadith in a specific book.")
 @app_commands.describe(keywords = "Enter the keywords of the hadith", book = "Enter book.",lang = "English or Arabic?")
 @app_commands.choices(lang = [
   Choice(name = "English", value = "1"),
@@ -138,10 +160,13 @@ async def search_in(interaction: discord.Interaction, keywords: str, book: str,l
           await interaction.followup.send(f"I found more than one narrations with those keywords, [click here]({hadith['link']}) to view them.")
 
 
-@tree.command(name = "madad", description = "Returns the bot's functions.", guild=discord.Object(id=1072270741501907094))
+@tree.command(name = "madad", description = "Returns the bot's functions.")
 async def madad(interaction: discord.Interaction):
   await interaction.response.send_message("'/hadith' function takes the name of a hadith book and the hadith number and returns the requested hadith from thaqalayn.\n'/search' function requires you enter the exact keywords of a hadith in english or arabic and returns the requested hadith from thaqalayn.\n'/search in' function lets you search for a query in a specific book\n/link' function requires you enter a valid link to a hadith on thaqalyn, and it returns that hadith.\n'/random' function just sends a random hadith lol")
 
 
 
-client.run(TOKEN)
+
+
+keep_alive()
+client.run("MTA3MjI3MTA0NzI1ODI4MDAwNw.Gk6ZWS.S5dz4I0YgaZfYVe5NjLS6XBIhxhFifqe5g6oOM")
